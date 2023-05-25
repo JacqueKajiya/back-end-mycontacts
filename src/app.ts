12be5d0c5ee8d74715fd34a011f1a2e67fdd5f9e
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-05-25 18:32:46 -0300

    feat: implemented data validations and error settings

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import "reflect-metadata";
 import "express-async-errors";
 import express, { Application } from "express";
 import { usersRoutes } from "./routes/users.routes";
+import handleAppErrorMiddleware from "./middlewares/handleAppError.middleware";
 
 const app: Application = express()
 app.use(express.json())
 
 app.use("/users", usersRoutes)
+
+app.use(handleAppErrorMiddleware)
 
 export default app;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createUserController } from "../controllers/users.controller";
+import { ensureDataIsValidMiddleware } from "../middlewares/ensureDataIsValid.middleware";
+import { userSchemaRequest } from "../schemas/users.schemas";
 
 const usersRoutes = Router()
 
-usersRoutes.post("", createUserController)
+usersRoutes.post("", ensureDataIsValidMiddleware(userSchemaRequest), createUserController)
 
 export { usersRoutes }
