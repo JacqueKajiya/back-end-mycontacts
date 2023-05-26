c21aefd9868cbc51bd497421f7e3aac8ae0ffc36
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-05-26 16:51:01 -0300

    feat: installed cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import "express-async-errors";
+import cors from 'cors';
 import express, { Application } from "express";
 import { usersRoutes } from "./routes/users.routes";
 import handleAppErrorMiddleware from "./middlewares/handleAppError.middleware";
@@ -7,6 +8,7 @@ import { loginRoutes } from "./routes/login.routes";
 import { contactRoutes } from "./routes/contacts.routes";
 
 const app: Application = express()
+app.use(cors())
 app.use(express.json())
 
 app.use("/users", usersRoutes)
