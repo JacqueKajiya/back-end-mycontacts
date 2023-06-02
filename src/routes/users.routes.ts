ca5967934ce3a35bbed6510caafb81f5154eec30
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-06-02 10:52:37 -0300

    fix: applied auth middleware in user route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { createUserController, listUserController } from "../controllers/users.controller";
 import { ensureDataIsValidMiddleware } from "../middlewares/ensureDataIsValid.middleware";
 import { userSchemaRequest } from "../schemas/users.schemas";
+import { ensureAuthMiddleware } from "../middlewares/ensureAuth.middleware";
 
 const usersRoutes = Router()
 
 usersRoutes.post("", ensureDataIsValidMiddleware(userSchemaRequest), createUserController)
-usersRoutes.get("", listUserController)
+usersRoutes.get("", ensureAuthMiddleware, listUserController)
 
 export { usersRoutes }
